7e2b7c37aff06d0486ba305a933934fbe3970110
shanurajput23 <shanurajput7631@gmail.com>
2023-06-16 15:42:46 +0530

    ok

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./home.scss"
+
+const Home=() => {
+    return (
+        <div className="Home">
+            <h1 className="title">lMS</h1>
+        
+        
+        </div>
+
+    )
+}
+
+export default Home

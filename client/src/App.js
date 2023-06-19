b777eb2129bfcb89fb38c98ac8d0d6df0cba90f1
19levi19 <amanchaudhur29902@gmail.com>
2023-06-19 11:47:31 +0530

    changed directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 
 import Home from "./pages/home/Home";
-import List from "./pages/List/List";
+// import List from "./pages/List/List";
 // import Single from "./pages/Single/Single";
 // import New from "./pages/New/New";
-import Courses from "./courses_catalog/courses";
+import Courses from "./pages/courses_catalog/courses";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -12,18 +12,10 @@ function App() {
        <Routes>
          <Route path="/">
           <Route index element={<Home />} />
-          <Route path="login" element={<login/>}/>
-          <Route path="users">
-            <Route index element={<List/>}/>
-            <Route path=":userId" element={<single/>}/>
-            <Route path="new" element={<new/>}/>  
-          </Route>
-          <Route path="Products">
-            <Route index element={<List/>}/>
-            <Route path=":productId" element={<single/>}/>
-            <Route path="new" element={<new/>}/>  
-          </Route>
-          <Route path="Courses">
+    
+          
+          
+          <Route path="Courses" >
             <Route index element={<Courses />}/>
           </Route>
         </Route>

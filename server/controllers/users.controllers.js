58f37bc67f6bef9ea5a53612e0273893ae97dca9
19levi19 <amanchaudhur29902@gmail.com>
2023-06-24 14:54:39 +0530

    created api for crud operations

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -1,7 +1,7 @@
 app.use(require('../mongodb/models/user'));
 
 const getAllusers = async (req,res) => {
-
+        
 };
 const createuser = async (req,res) => {};
 const getUserInfoByID = async (req,res) => {};
